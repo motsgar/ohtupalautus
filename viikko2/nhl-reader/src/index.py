f13d717bccd6a1c6dd28490ec29588505f490b72
Oskar Vihriälä <oskar.vihriala@gmail.com>
2024-11-11 21:58:27 +0200

    Refactor to dependency injection

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,21 +1,12 @@
-import requests
-from player import Player
+from player import PlayerStats, PlayerReader
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    fin_players = Player.filter_players_by_nationality(players, "FIN")
-
-    print(f"Players from FIN: {len(fin_players)}")
-
-    for player in fin_players:
+    for player in players:
         print(player)
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,17 +1,43 @@
+import requests
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+        players = []
+
+        for player_dict in response:
+            player = Player(player_dict["name"],
+                            player_dict["nationality"],
+                            player_dict["assists"],
+                            player_dict["goals"],
+                            player_dict["team"],
+                            player_dict["games"],
+                            player_dict["id"])
+            
+            players.append(player)
+
+        return players
+
+class PlayerStats:
+    def __init__(self, playerReader):
+        self.players = playerReader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        return [player for player in self.players if player.nationality == nationality]
+
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.nationality = dict['nationality']
-        self.assists = dict['assists']
-        self.goals = dict['goals']
-        self.team = dict['team']
-        self.games = dict['games']
-        self.id = dict['id']
+    def __init__(self, name, nationality, assists, goals, team, games, id):
+        self.name = name
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.team = team
+        self.games = games
+        self.id = id
+        
     
     def __str__(self):
         return f"{self.name:20} team {self.team:3} {self.goals:2} + {self.assists:2} = {self.goals + self.assists:2}"
-    
-
-    @staticmethod
-    def filter_players_by_nationality(players, nationality):
-        return [player for player in players if player.nationality == nationality]
